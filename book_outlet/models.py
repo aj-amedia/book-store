17f5465fd28444797566014fcce78cf093618adf
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2025-12-04 11:04:39 -0700

    Register book_outlet app; Register Book model

---
+++ book_outlet/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    # id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+
