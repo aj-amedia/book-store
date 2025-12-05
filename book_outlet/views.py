64f682c963b411efc3ff29aa93ad1c5f9b54d632
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2025-12-05 13:53:27 -0700

    Add book details, slug, aggregation

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.http import HttpResponse, Http404
+from django.db.models import Avg, Min
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    agg_rating = books.aggregate(Avg('rating'), Min('rating')) # rating__avg, rating__min
+
+
+    return render(request, 'book_outlet/index.html', {
+        'books': books,
+        'total_number_of_books': num_books,
+        'agg_rating': agg_rating
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, 'book_outlet/book_detail.html', {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_best_seller": book.is_best_selling,
+    })
