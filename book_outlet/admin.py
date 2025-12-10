e5d9adb829e40db5ba83f52f084955f6beae34bb
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2025-12-10 10:39:45 -0700

    Add cross model queries

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 
@@ -8,5 +8,7 @@ class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug", )
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ('author', 'rating')
+    list_display = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
